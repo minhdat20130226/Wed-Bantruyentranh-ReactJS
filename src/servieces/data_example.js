6222d1ead694f64a1c370b38a36944bc666a8fdb
minhdat20130226 <20130226@st.hcmuaf.edu.vn>
2024-04-12 04:47:55 +0700

    slide banner & update header top

---
+++ src/servieces/data_example.js
@@ -0,0 +1,66 @@
+
+
+export const DANHMUC = {
+  "danh_muc_truyen": [
+    {
+      "ten": "Truyện tiên hiệp",
+      "sach": [
+        {
+          "ten_sach": "Thiên long bát bộ",
+          "nha_xuat_ban": "Kim Đồng",
+          "tac_gia": "Kim Dung"
+        },
+        {
+          "ten_sach": "Tiên hiệp thế giới",
+          "nha_xuat_ban": "Nhà xuất bản Trẻ",
+          "tac_gia": "Nguyễn Hùng Lân"
+        }
+      ]
+    },
+    {
+      "ten": "Truyện anime",
+      "sach": [
+        {
+          "ten_sach": "Naruto",
+          "nha_xuat_ban": "Shueisha",
+          "tac_gia": "Masashi Kishimoto"
+        },
+        {
+          "ten_sach": "One Piece",
+          "nha_xuat_ban": "Shueisha",
+          "tac_gia": "Eiichiro Oda"
+        }
+      ]
+    },
+    {
+      "ten": "Truyện hài hước",
+      "sach": [
+        {
+          "ten_sach": "Gintama",
+          "nha_xuat_ban": "Shueisha",
+          "tac_gia": "Hideaki Sorachi"
+        },
+        {
+          "ten_sach": "Crayon Shin-chan",
+          "nha_xuat_ban": "Futabasha",
+          "tac_gia": "Yoshito Usui"
+        }
+      ]
+    },
+    {
+      "ten": "Truyện phiêu lưu",
+      "sach": [
+        {
+          "ten_sach": "Harry Potter",
+          "nha_xuat_ban": "Bloomsbury",
+          "tac_gia": "J.K. Rowling"
+        },
+        {
+          "ten_sach": "Percy Jackson & the Olympians",
+          "nha_xuat_ban": "Disney Hyperion",
+          "tac_gia": "Rick Riordan"
+        }
+      ]
+    }
+  ]
+}
