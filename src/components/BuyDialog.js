3e31e108fb036116ef58873e30a6ec6e13660b42
minhdat20130226 <20130226@st.hcmuaf.edu.vn>
2024-04-17 03:50:56 +0700

    update topic group and buy dialog

--- src/components/BuyDialog.js
+++ src/components/BuyDialog.js
@@ -1,28 +1,49 @@
 import React, { Component } from 'react';
 import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import Modal from 'react-bootstrap/Modal';
-import { Container, Card, ListGroup } from 'react-bootstrap';
+import { Container, Card, } from 'react-bootstrap';
 
 class BuyDialog extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            isHovered: false // Trạng thái của con trỏ chuột
+        };
     }
 
+    handleMouseEnter = () => {
+        this.setState({ isHovered: true });
+    }
+
+    handleMouseLeave = () => {
+        this.setState({ isHovered: false });
+    }
+    
     render() {
-        const { showBuyDialog, onHide, alignLeftPosition } = this.props;
+        const { showBuyDialog, alignLeftPosition } = this.props;
+        console.log(this.state.isHovered)
         return (
-            // onMouseLeave={onHide}
             <>
-                <div className='buy-dialog'  onMouseLeave={onHide} style={{display:`${showBuyDialog?'block':'block'}`,left:`${alignLeftPosition}px`}}>     
-                    <Card >
+                <div
+                    className='buy-dialog'
+                    onMouseEnter={this.handleMouseEnter}
+                    onMouseLeave={this.handleMouseLeave}         
+                    style={{                      
+                        left: `${alignLeftPosition}%`
+                    }}        
+                >
+                    <Card
+                    style={{
+                        display: `${this.state.isHovered ? 'block' : 'none'}`,
+                        left: `${225}px`
+                    }}
+                    >
                         <Card.Body>
                             <Card.Title className=''>Custom Modal Styling</Card.Title>
                             <div className='des-txt'>
                                 <p>
-                                Cọ Stylus SilStar BuTouch Professional Hàn Quốc Tương Thích Android, iOS Butouch là thương hiệu cọ stylus của hãng SilStar (Hàn Quốc) với sứ mệnh mang lợi ích của vẽ 
-                                tranh kỹ thuật số (digital painting) đến mọi ngườCọ Stylus SilStar BuTouch Professional Hàn Quốc Tương Thích Android, iOS Butouch là thương hiệu cọ stylus của hãng SilStar 
-                                (Hàn Quốc) với sứ mệnh mang lợi ích của vẽ tranh kỹ thuật số (digital painting) đến mọi người, bất kể tuổi tác hay cấp độ sử dụng. Từ Năm 2013i, bất kể tuổi tác hay cấp độ sử dụng. Từ Năm 2013
+                                    Cọ Stylus SilStar BuTouch Professional Hàn Quốc Tương Thích Android, iOS Butouch là thương hiệu cọ stylus của hãng SilStar (Hàn Quốc) với sứ mệnh mang lợi ích của vẽ
+                                    tranh kỹ thuật số (digital painting) đến mọi ngườCọ Stylus SilStar BuTouch Professional Hàn Quốc Tương Thích Android, iOS Butouch là thương hiệu cọ stylus của hãng SilStar
+                                    (Hàn Quốc) với sứ mệnh mang lợi ích của vẽ tranh kỹ thuật số (digital painting) đến mọi người, bất kể tuổi tác hay cấp độ sử dụng. Từ Năm 2013i, bất kể tuổi tác hay cấp độ sử dụng. Từ Năm 2013
                                 </p>
                                 <a href='#'>Xem thêm</a>
                             </div>
