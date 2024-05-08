fb6a80fb6851761ed06b6a8a27a990c976249cfc
minhdat20130226 <20130226@st.hcmuaf.edu.vn>
2024-05-08 21:28:31 +0700

    update home page

--- src/containers/HomePage/Banner/Banner.js
+++ src/containers/HomePage/Banner/Banner.js
@@ -1,17 +1,58 @@
 import React, { Component } from 'react';
-import {Carousel, Container,Image } from 'react-bootstrap';
+import { Carousel, Container, Image, Dropdown, DropdownButton } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { PATH } from '../../../utils/constant';
-import ProductCartAdd from '../../../components/ProductCartAdd';
+import categoryService from '../../../servieces/categoryService';
 
+const positionTop = [0, -42, -84, -126, -168, -210, -252, -294];
 class Banner extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      activeButtonIndex: 0 // Mặc định là button đầu tiên
-    }; 
+      activeButtonIndex: 0,
+      showMenu: false,
+      hoveredDanhMuc: null,
+      categoryGenresData: {},
+      dataGenre: {},
+
+    };
+  }
+  handleMouseEnter = (tenDanhMuc) => {
+
+    this.setState({
+      showMenu: true,
+      hoveredDanhMuc: tenDanhMuc
+    });
   }
 
+  handleMouseLeave = () => {
+    this.setState({
+      showMenu: false,
+      hoveredDanhMuc: null
+    });
+  }
+
+  async componentDidMount() {
+    const currentPath = window.location.pathname;
+    const showNavbar = currentPath === PATH.HOME;
+    this.setState({ showNavbar });
+    try {
+      const responseData = await categoryService.getCategoryGenres();
+      this.setState({ categoryGenresData: responseData });
+    } catch (error) {
+      console.error('Service Error:', error);
+    }
+  }
+
+  handleItemClick = (genre) => {
+    try {
+      const pathSub = genre.replace(/\s+/g, "-");
+      const navigate = this.props.navigate;
+      navigate(`${PATH.SEARCHBOOKPAGE}/${pathSub}`, { state: { dataGenre: "qưe" } });
+    } catch (error) {
+      console.error('Error data:', error)
+    }
+  }
   handleSlide = (selectedIndex, e) => {
     this.setState({ activeButtonIndex: selectedIndex }); // Cập nhật trạng thái vị trí của button được kích hoạt
   };
@@ -19,6 +60,57 @@ class Banner extends Component {
   handleSpanClick = (index) => {
     this.setState({ activeButtonIndex: index }); // Cập nhật trạng thái vị trí của button tương ứng với span được click
   };
+
+  renderCategory(danh_muc, categoryGenresData) {
+    return (
+      <>
+        {danh_muc.map((danh_muc, index) => (
+          <DropdownButton
+            key={index}
+            id={`dropdown-${danh_muc}-button`}
+            className="item"
+            drop="end"
+            href={`${PATH.SEARCHBOOKPAGE}/${danh_muc.replace(/\s+/g, "-")}`}
+            title={danh_muc}
+            show={this.state.showMenu && this.state.hoveredDanhMuc === danh_muc}
+            onMouseEnter={() => this.handleMouseEnter(danh_muc)}
+            onMouseLeave={this.handleMouseLeave}
+          // onClick={() => this.handleItemClick(danh_muc)}
+          >
+            <div className="menu-box-item" style={{ display: 'flex', top: `${positionTop[index]}px` }}>
+              <div>
+                <Dropdown.Header>Nổi bật</Dropdown.Header>
+                {categoryGenresData[danh_muc].slice(0, 7).map((item, index) => (
+                  <Dropdown.Item key={index} as='a' href={`${PATH.SEARCHBOOKPAGE}/${danh_muc.replace(/\s+/g, "-")}/${item.storyAuthorName.replace(/\s+/g, "-")}`}>{item.storyAuthorName}</Dropdown.Item>
+                ))}
+              </div>
+              <div>
+                <Dropdown.Header>Dịch giả</Dropdown.Header>
+                {categoryGenresData[danh_muc].slice(0, 6).map((item, index) => (
+                  <Dropdown.Item key={index} as='a' href={`${PATH.SEARCHBOOKPAGE}/${danh_muc.replace(/\s+/g, "-")}/${item.storyTranslatorName.replace(/\s+/g, "-")}`}>{item.storyTranslatorName}</Dropdown.Item>
+                ))}
+              </div>
+              <div>
+                <Dropdown.Header>Nhà xuất bản</Dropdown.Header>
+                {categoryGenresData[danh_muc].slice(0, 7).map((item, index) => (
+                  <Dropdown.Item key={index} as='a' href={`${PATH.SEARCHBOOKPAGE}/${danh_muc.replace(/\s+/g, "-")}/${item.placePublicationName.replace(/\s+/g, "-")}`}>{item.placePublicationName}</Dropdown.Item>
+                ))}
+              </div>
+              <div>
+                <Dropdown.Header>Tác giả</Dropdown.Header>
+                {categoryGenresData[danh_muc].slice(0, 6).map((item, index) => (
+                  <Dropdown.Item key={index} as='a' href={`${PATH.SEARCHBOOKPAGE}/${danh_muc.replace(/\s+/g, "-")}/${item.storyAuthorName.replace(/\s+/g, "-")}`}>{item.storyAuthorName}</Dropdown.Item>
+                ))}
+              </div>
+            </div>
+          </DropdownButton>
+        ))}
+      </>
+    )
+  }
+
+
+
 
 
   render() {
@@ -29,33 +121,36 @@ class Banner extends Component {
       { nameIndex: 'TRUYỆN MỚI', img: "https://bookbuy.vn/Res/Images/Album/d425cbf2-4fbf-49c0-8b5c-f3f14bc10992.jpg?w=880&scale=both&h=320&mode=crop" },
       { nameIndex: 'QUÀ TẶNG', img: "https://bookbuy.vn/Res/Images/Album/3dc9db62-b9b6-4f44-a584-c67eaa332c31.jpg?w=880&scale=both&h=320&mode=crop" },
     ];
-
+    const { categoryGenresData } = this.state;
+    const danh_muc = Object.keys(categoryGenresData);
     return (
       <div className='container-banner'>
         <Container>
-        <div style={{width:'240px',marginRight:'10px',backgroundColor:'#fff'}}></div>
-        <div className=''>
-          <div className='main-slide'>
-            <Carousel data-bs-theme="dark" controls={false} activeIndex={this.state.activeButtonIndex} onSelect={this.handleSlide} direction="prev">
+          <div className='menu-product'>
+            {this.renderCategory(danh_muc, categoryGenresData)}
+          </div>
+          <div className=''>
+            <div className='main-slide'>
+              <Carousel data-bs-theme="dark" controls={false} activeIndex={this.state.activeButtonIndex} onSelect={this.handleSlide} direction="prev">
+                {carouselItems.map((item, index) => (
+                  <Carousel.Item key={index} interval={1200} >
+                    <Link className="nav-link" to={PATH.PROMOTIONPROGRAM} target='blank'>
+                      {/* <img className="d-block" src={item.img} alt={`Slide ${index + 1}`} /> */}
+                      <Image className="d-block" src={item.img} alt={`Slide ${index + 1}`}></Image>
+                    </Link>
+                  </Carousel.Item>
+                ))}
+              </Carousel>
+
+            </div>
+            <div className='control-silde'>
               {carouselItems.map((item, index) => (
-                <Carousel.Item key={index} interval={1200} >
-                  <Link className="nav-link" to={PATH.PROMOTIONPROGRAM} target='blank'>
-                    {/* <img className="d-block" src={item.img} alt={`Slide ${index + 1}`} /> */}
-                    <Image className="d-block" src={item.img} alt={`Slide ${index + 1}`}></Image>
-                  </Link>
-                </Carousel.Item>
+                <span key={index} className={index === this.state.activeButtonIndex ? 'active' : ''} onClick={() => this.handleSpanClick(index)}>
+                  {item.nameIndex}
+                </span>
               ))}
-            </Carousel>
-          
+            </div>
           </div>
-          <div className='control-silde'>
-          {carouselItems.map((item, index) => (
-              <span key={index} className={index === this.state.activeButtonIndex ? 'active' : ''} onClick={() => this.handleSpanClick(index)}>
-                {item.nameIndex}
-              </span>
-            ))}
-          </div>
-        </div>
         </Container>
       </div>
     );
